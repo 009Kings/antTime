3a015866135f1f2f8e18ba0ed0088a210cb1c1ae
sross9845 <sean.ross.985@gmail.com>
2019-09-23 20:35:43 -0700

    Obviously superior changes

--- app.js
+++ app.js
@@ -3,9 +3,17 @@ let lights = document.querySelectorAll(".light")
 let colors = ["red", "yellow", "green"]
 let go = true;
 let movinAnt = document.getElementById("movin")
+let movinAnt2 = document.getElementById("movin2")
+let movinAnt3 = document.getElementById("movin3")
+let movinAnt4 = document.getElementById("movin4")
+let movinAnt5 = document.getElementById("movin5")
 let movement = 0
 // true is forward
 let forward = true
+let forward2 = true
+let forward3 = true
+let forward4 = true
+let forward5 = true
 
 let clearColors = function() {
   // change all to grey
@@ -31,18 +39,38 @@ let loop = setInterval(function () {
   if (go) {
     if (movement > 100 || movement < 0) {
       forward = !forward
-      movinAnt.src = `assets/ant-${forward}.png`
+      forward2 = !forward2
+      forward3 = !forward3
+      forward4 = !forward4
+      forward5 = !forward5
+      movinAnt.src = `assets/alert-${forward}.png`
+      movinAnt2.src = `assets/alert-${forward}.png`
+      movinAnt3.src = `assets/alert-${forward}.png`
+      movinAnt4.src = `assets/alert-${forward}.png`
+      movinAnt5.src = `assets/alert-${forward}.png`
       console.log(movinAnt.src)
     }
     forward ? movement+=3 : movement -=3
     movinAnt.style.left = `${movement}vw`
+    forward2 ? movement+=2 : movement -=2
+    movinAnt2.style.left = `${movement}vw`
+    forward3 ? movement+=3 : movement -=3
+    movinAnt3.style.left = `${movement}vw`
+    forward4 ? movement+=4 : movement -=4
+    movinAnt4.style.left = `${movement}vw`
+    forward5 ? movement+=4 : movement -=4
+    movinAnt5.style.left = `${movement}vw`
   } 
 }, 100)
 
 button.addEventListener("click", function () {
-  changeColors()
-})
+  alert(`SO YOU THOUGHT!!! YOU CAN NEVER STOP THESE SWEET SWEET ALERTS`)
+  alert(`WE ALL LOVE ALERTS`)
+  alert(`YOU ARE NOW PART OF THE ALERTS FAN CLUB`)
+});
 
 document.getElementById("stop").addEventListener("click", function () {
-  clearInterval(loop)
+  alert(`HA THEY ARE NEVER ANNOYING AND WILL NEVER STOP`)
+  alert(`EMBRACE THE ALERTS`)
+  alert(`LOOK AT THESE SWEET SWEET ALERTS`)
 })
